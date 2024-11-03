bf3707923e0c0725d19b7c838886ff9e7d0864c5
Joseph Hardwick <josephhardwick@Joes-MacBook-Air.local>
2024-11-03 14:48:23 -0500

    updated actions 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <p>If you're seeing this github actions worked! 3</p>
-      <p>Atomic Actions! woahh!!</p>
+      <p>Atomic Actions! woahh!! (test 2)</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="../images/Baxter.jpg"
